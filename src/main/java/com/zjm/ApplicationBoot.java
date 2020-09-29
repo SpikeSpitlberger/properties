f2fcfa6fb112cf9921f1f2991fc720f2efbe34f5
SpikeZhu <761029267@qq.com>
2020-09-29 19:43:30 +0800

    新添加文件

---
+++ src/main/java/com/zjm/ApplicationBoot.java
@@ -0,0 +1,12 @@
+package com.zjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ApplicationBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBoot.class, args);
+    }
+}
--- src/main/java/com/zjm/utils/PropertiesTool.java
+++ src/main/java/com/zjm/utils/PropertiesTool.java
@@ -1,4 +1,10 @@
 package com.zjm.utils;
 
+import java.util.LinkedHashMap;
+import java.util.Properties;
+
 public class PropertiesTool {
+    private final LinkedHashMap<String, String> keyCommentMap = new LinkedHashMap<String, String>();
+    private static final String BLANK = "";
+    private static final char[] hexDigit = new char[]{'0', '1', '2', '3',};
 }
